9d296cb2b66d522787f20b30bc8016a36af7af37
imana173 <imana173@gmail.com>
2024-11-27 16:12:20 +0100

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors'); // Importer le middleware CORS
 const connectDB = require('./config/configdb'); // Connexion à MongoDB
 
 const authRouter = require('./router/auth'); // Routeur pour l'authentification
@@ -12,9 +13,15 @@ const port = 5000; // Définir le port d'écoute
 // Connexion à MongoDB
 connectDB();
 
+// Middleware CORS
+app.use(cors({
+  origin: "https://frontgencv-project.onrender.com", // URL du frontend déployé
+  methods: ["GET", "POST", "PATCH", "DELETE"], // Méthodes HTTP autorisées
+  allowedHeaders: ["Content-Type", "Authorization"], // En-têtes autorisés
+}));
+
 // Middleware pour analyser les données JSON
 app.use(express.json());
-
 
 // Routes pour l'authentification
 app.use('/auth', authRouter);
@@ -22,13 +29,15 @@ app.use('/auth', authRouter);
 // Routes pour les CV
 app.use('/api/cv', cvRouter);
 
+// Routes pour les recommandations
 app.use('/api/recommendations', recommendationRouter);
+
 // Ajouter les routes utilisateur
 app.use('/api/user', userRouter);
-
 
 // Démarrer le serveur
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
 });
+
 
